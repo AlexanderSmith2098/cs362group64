c2da36a1611568c404892356abc18a0cb97c52ec
AlexanderSmith2098 <smitale2@oregonstate.edu>
2020-03-08 12:27:43 -0700

    file tweak

--- group64/UnitTestAndCode/test_calculator.py
+++ group64/UnitTestAndCode/test_calculator.py
@@ -5,8 +5,48 @@ import math
 #Must work on whole numbers, fractions, negative, large, and small
 
 class TestCase(unittest.TestCase):
-
-
+    def testmultiplication(self):
+        input1a = 99999999999
+        input1b = -99999999999
+        expected1 = input1a * input1b
+        input2a = -0.0000000001
+        input2b = -0.0000000001
+        expected2 = input2a * input2b
+        input3a = 0
+        input3b = 64
+        expected3 = input3a * input3b
+        self.assertEqual(expected1, calculator.multiplication(input1a,input1b))
+        self.assertEqual(expected2, calculator.multiplication(input2a,input2b))
+        self.assertEqual(expected3, calculator.multiplication(input3a,input3b))
+        
+    def testdivision(self):
+        input1a = 99999999999
+        input1b = -99999999999
+        expected1 = input1a / input1b
+        input2a = -0.0000000001
+        input2b = -0.0000000001
+        expected2 = input2a / input2b
+        input3a = 0
+        input3b = 64
+        expected3 = input3a / input3b
+        expected4 = False
+        self.assertEqual(expected1, calculator.division(input1a,input1b))
+        self.assertEqual(expected2, calculator.division(input2a,input2b))
+        self.assertEqual(expected3, calculator.division(input3a,input3b))
+        self.assertEqual(expected4, calculator.division(input3b,input3a))
+        
+    def testsquare_root(self):
+        input1 = 99999999999
+        expected1 = math.sqrt(input1)
+        input2 = 0.0000000001
+        expected2 = math.sqrt(input2)
+        input3 = 64
+        expected3 = math.sqrt(input3)
+        expected4 = False
+        self.assertEqual(expected1, calculator.square_root(input1))
+        self.assertEqual(expected2, calculator.square_root(input2))
+        self.assertEqual(expected3, calculator.square_root(input3))
+        self.assertEqual(expected4, calculator.square_root(input3 * -1))
 
 
     def testabs(self):
@@ -24,9 +64,9 @@ class TestCase(unittest.TestCase):
         input1 = 0
         expected1 = 0
         input2 = -(math.pi)
-        expected2 = math.sin(input2)
+        expected2 = 0
         input3 = math.pi / 2
-        expected3 = math.sin(input3)
+        expected3 = 1
         self.assertEqual(expected1, calculator.sin(input1))
         self.assertEqual(expected2, calculator.sin(input2))
         self.assertEqual(expected3, calculator.sin(input3))
@@ -36,9 +76,9 @@ class TestCase(unittest.TestCase):
         input1 = 0
         expected1 = 1
         input2 = -(math.pi)
-        expected2 = -1.0
+        expected2 = 1
         input3 = math.pi / 2
-        expected3 = math.cos(input3)
+        expected3 = 0
         self.assertEqual(expected1, calculator.cos(input1))
         self.assertEqual(expected2, calculator.cos(input2))
         self.assertEqual(expected3, calculator.cos(input3))
