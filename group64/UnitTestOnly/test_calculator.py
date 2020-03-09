0315137948984ba1bea4ce3f860f16b55c55f228
Daniel Tran <dtran550@gmail.com>
2020-03-09 12:15:26 -0700

    Added tests for square, multInverse, factorial

--- group64/UnitTestOnly/test_calculator.py
+++ group64/UnitTestOnly/test_calculator.py
@@ -50,59 +50,77 @@ class TestCase(unittest.TestCase):
         self.assertEqual(expected4, calculator.square_root(input3 * -1))
 
     def test_squareNum(self):
+        # Partition testing variables
         input_zero = 0
         input_positive = 500
         input_negative = -495
         input_fraction = .7
+        # Boundary testing variables
         input_posBoundary = 9999999999999999
         posExpected = 9999999999999999 * 9999999999999999
         input_negBoundary = -9999999999999999
         negExpected = -9999999999999999 * -9999999999999999
         input_beyondBoundary = 10000000000000000000
+        # Bad input testing variable
         input_badInput = "dfjaldkf"
+
         self.assertEqual(0, calculator.squareNum(input_zero))
         self.assertEqual(250000, calculator.squareNum(input_positive))
         self.assertEqual(245025, calculator.squareNum(input_negative))
+        # Almost equal utilized because of float representation not providing 1:1 correspondence with real life
+        # calculation of decimal multiplication
         self.assertAlmostEqual(0.49, calculator.squareNum(input_fraction))
+        # Boundary assert statements and error checking
         self.assertEqual(posExpected, calculator.squareNum(input_posBoundary))
         self.assertEqual(negExpected, calculator.squareNum(input_negBoundary))
         self.assertEqual("Error, number of digits in input too large", calculator.squareNum(input_beyondBoundary))
         self.assertEqual("Error, only numerical input allowed", calculator.squareNum(input_badInput))
 
     def test_multInverse(self):
+        # Partition testing variables
         input_positive = 500
         input_negative = -495
         negExpected = -0.0020202
         input_fraction = .40
+        # Boundary testing variables
         input_zero = 0
         input_atBoundary = 9999999999999999
         boundaryExpected = 1 / 9999999999999999
         input_beyondBoundary = 10000000000000000000
+        # Bad character testing variable
         input_badInput = "dfjaldkf"
+        
         self.assertEqual(0.002, calculator.multInverse(input_positive))
+        # Almost equal assert utilized due to infinite repeating end sequence
         self.assertAlmostEqual(negExpected, calculator.multInverse(input_negative))
         self.assertEqual(2.5, calculator.multInverse(input_fraction))
+        # Boundary assert statements
         self.assertEqual("Error, division by 0", calculator.multInverse(input_zero))
         self.assertEqual(boundaryExpected, calculator.multInverse(input_atBoundary))
         self.assertEqual("Error, number of digits in input too large", calculator.multInverse(input_beyondBoundary))
         self.assertEqual("Error, only numerical input allowed", calculator.multInverse(input_badInput))
 
     def test_fact(self):
-        input0 = 0
-        input1 = 12
-        input2 = 30
-        input3 = 100
+        # Input variables to test normal use of fact function
         input_fraction = 0.34
         expectedfractionFact = 0.8922155
+        input1 = 12
+        input2 = 30
+        # Boundary testing and error checking variables
+        input3 = 100
         input_negative = -495
+        inputZero = 0
         input_beyondBoundary = 101
         input_badInput = "dfjaldkf"
-        self.assertEqual(1, calculator.fact(input0))
+
+        # Almost equal used because gamma function for fractional factorial has infinite sequence of trailing numbers
+        self.assertAlmostEqual(expectedfractionFact, calculator.fact(input_fraction))
         self.assertEqual(479001600, calculator.fact(input1))
         self.assertEqual(265252859812191058636308480000000, calculator.fact(input2))
+        # Used math.factorial(), since the numerical representation is very long as expected value, and boundary asserts
         self.assertEqual(math.factorial(100), calculator.fact(input3))
-        self.assertAlmostEqual(expectedfractionFact, calculator.fact(input_fraction))
         self.assertEqual("Non-negative numbers only", calculator.fact(input_negative))
+        self.assertEqual(1, calculator.fact(inputZero))
         self.assertEqual("Overflow", calculator.fact(input_beyondBoundary))
         self.assertEqual("Error, only numerical input allowed", calculator.fact(input_badInput))
 
